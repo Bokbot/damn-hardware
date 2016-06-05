1b49188afa0405dcf55637b7612a91fa1e67ee67
Josh Cox <josh@webhosting.coop>
2016-06-05 06:59:41 -0500

    hrm3

---
+++ hrm2/hrm2.ino
@@ -0,0 +1,169 @@
+/*
+   Copyright (c) 2015 Intel Corporation.  All rights reserved.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+   This sketch example partially implements the standard Bluetooth Low-Energy Heart Rate service.
+   For more information: https://developer.bluetooth.org/gatt/services/Pages/ServicesHome.aspx
+*/
+volatile int rate[10];                    // used to hold last ten IBI values
+volatile unsigned long sampleCounter = 0;          // used to determine pulse timing
+#include "CurieIMU.h"
+volatile int P =512;                      // used to find peak in pulse wave
+volatile int T = 512;                     // used to find trough in pulse wave
+volatile int thresh = 512;                // used to find instant moment of heart beat
+volatile int amp = 100;                   // used to hold amplitude of pulse waveform
+volatile boolean firstBeat = true;        // used to seed rate array so we startup with reasonable BPM
+volatile boolean secondBeat = true;       // used to seed rate array so we startup with reasonable BPM
+
+
+void interruptSetup(){     
+  // Initializes Timer2 to throw an interrupt every 2mS.
+  TCCR2A = 0x02;     // DISABLE PWM ON DIGITAL PINS 3 AND 11, AND GO INTO CTC MODE
+  TCCR2B = 0x06;     // DON'T FORCE COMPARE, 256 PRESCALER 
+  OCR2A = 0X7C;      // SET THE TOP OF THE COUNT TO 124 FOR 500Hz SAMPLE RATE
+  TIMSK2 = 0x02;     // ENABLE INTERRUPT ON MATCH BETWEEN TIMER2 AND OCR2A
+  sei();             // MAKE SURE GLOBAL INTERRUPTS ARE ENABLED      
+} 
+
+
+#include <CurieBLE.h>
+#include <CurieIMU.h>
+
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 13;                // pin to blink led at each beat
+int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
+int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
+
+
+// these variables are volatile because they are used during the interrupt service routine!
+volatile int BPM;                   // used to hold the pulse rate
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // holds the time between beats, the Inter-Beat Interval
+volatile boolean Pulse = false;     // true when pulse wave is high, false when it's low
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+BLEPeripheral blePeripheral;       // BLE Peripheral Device (the board you're programming)
+BLEService heartRateService("180D"); // BLE Heart Rate Service
+
+// BLE Heart Rate Measurement Characteristic"
+BLECharacteristic heartRateChar("2A37",  // standard 16-bit characteristic UUID
+    BLERead | BLENotify, 2);  // remote clients will be able to get notifications if this characteristic changes
+                              // the characteristic is 2 bytes long as the first field needs to be "Flags" as per BLE specifications
+                              // https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
+
+int oldHeartRate = 0;  // last heart rate reading from analog input
+long previousMillis = 0;  // last time the heart rate was checked, in ms
+
+float convertRawAcceleration(int aRaw) {
+  // since we are using 2G range
+  // -2g maps to a raw value of -32768
+  // +2g maps to a raw value of 32767
+  
+  float a = (aRaw * 2.0) / 32768.0;
+
+  return a;
+}
+
+
+void ledFadeToBeat(){
+    fadeRate -= 15;                         //  set LED fade value
+    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
+    analogWrite(fadePin,fadeRate);          //  fade LED
+  }
+
+
+void sendDataToProcessing(char symbol, int data ){
+    Serial.print(symbol);                // symbol prefix tells Processing what type of data is coming
+    Serial.println(data);                // the data to send culminating in a carriage return
+  }
+
+
+void setup() {
+  Serial.begin(9600); // initialize Serial communication
+  while (!Serial);    // wait for the serial port to open
+
+  // initialize device
+  Serial.println("Initializing IMU device...");
+  CurieIMU.begin();
+
+  // Set the accelerometer range to 2G
+  CurieIMU.setAccelerometerRange(2);
+  pinMode(13, OUTPUT);   // initialize the LED on pin 13 to indicate when a central is connected
+
+  /* Set a local name for the BLE device
+     This name will appear in advertising packets
+     and can be used by remote devices to identify this BLE device
+     The name can be changed but maybe be truncated based on space left in advertisement packet */
+  blePeripheral.setLocalName("HeartRateSketch");
+  blePeripheral.setAdvertisedServiceUuid(heartRateService.uuid());  // add the service UUID
+  blePeripheral.addAttribute(heartRateService);   // Add the BLE Heart Rate service
+  blePeripheral.addAttribute(heartRateChar); // add the Heart Rate Measurement characteristic
+
+  /* Now activate the BLE device.  It will start continuously transmitting BLE
+     advertising packets and will be visible to remote BLE central devices
+     until it receives a new connection */
+  blePeripheral.begin();
+  Serial.println("Bluetooth device active, waiting for connections...");
+  pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
+  pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
+  Serial.begin(115200);             // we agree to talk fast!
+  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS 
+   // UN-COMMENT THE NEXT LINE IF YOU ARE POWERING The Pulse Sensor AT LOW VOLTAGE, 
+   // AND APPLY THAT VOLTAGE TO THE A-REF PIN
+   //analogReference(EXTERNAL);   
+}
+void updateHeartRate() {
+  /* Read the current voltage level on the A0 analog input pin.
+     This is used here to simulate the heart rate's measurement.
+  */
+  int heartRateMeasurement = analogRead(A0);
+  int heartRate = map(heartRateMeasurement, 0, 1023, 0, 100);
+  if (heartRate != oldHeartRate) {      // if the heart rate has changed
+    Serial.print("Heart Rate is now: "); // print it
+    Serial.println(heartRate);
+    const unsigned char heartRateCharArray[2] = { 0, (char)heartRate };
+    heartRateChar.setValue(heartRateCharArray, 2);  // and update the heart rate measurement characteristic
+    oldHeartRate = heartRate;           // save the level for next comparison
+  }
+}
+
+void loop() {
+  int axRaw, ayRaw, azRaw;         // raw accelerometer values
+  float ax, ay, az;
+
+  // read raw accelerometer measurements from device
+  CurieIMU.readAccelerometer(axRaw, ayRaw, azRaw);
+
+  // convert the raw accelerometer data to G's
+  ax = convertRawAcceleration(axRaw);
+  ay = convertRawAcceleration(ayRaw);
+  az = convertRawAcceleration(azRaw);
+
+  // display tab-separated accelerometer x/y/z values
+  Serial.print(ax);
+  Serial.print(",");
+  Serial.print(ay);
+  Serial.print(",");
+  Serial.print(az);
+  Serial.print(",");
+  Serial.print(ax);
+  Serial.print(BPM);
+  Serial.println();
+  sendDataToProcessing('B',BPM);   // send heart rate with a 'B' prefix
+
+}
